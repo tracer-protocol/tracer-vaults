d61542be285302da7283cc20ff8def5852476a75
0xkoda <koda_0x@protonmail.com>
2022-03-01 07:22:53 +1000

    Removed Ownable mod, imported OZ Ownable, removed balance tracking

--- contracts/EVault.sol
+++ contracts/EVault.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@rari-capital/solmate/src/tokens/ERC20.sol";
 import "@rari-capital/solmate/src/utils/SafeTransferLib.sol";
-//important! Importing this library from solmate will cause the contract to fail compile
+
+//important! Importing ERRC4626 from solmate will cause the contract to fail compile
 import "./utils/ERC4626.sol";
 import "./utils/FixedPointMathLib.sol";
 
@@ -12,11 +14,8 @@ import "./utils/FixedPointMathLib.sol";
 // BUFFER is the minimun amount of tokens that can be stored in the vault and should
 // be compared with bufBal to determine if the vault neeed topup
 // TOP_UP indicates if funds are needed from bot to facilitate a large withdrawal
- contract EVault is ERC4626 {
+ contract EVault is ERC4626, Ownable {
     using SafeTransferLib for ERC20;
-    //lets track balances for now
-    mapping(address => uint256) balances;
-    address payable public owner;
 
     //the underlying token the vault accepts
     ERC20 public immutable UNDERLYING;
@@ -29,12 +28,7 @@ import "./utils/FixedPointMathLib.sol";
 
     constructor(ERC20 underlying) ERC4626(underlying, "EVault", "EVLT") {
         UNDERLYING = ERC20(underlying);
-        owner = payable(msg.sender);
-    }
-
-    modifier onlyOwner() {
-        require(isOwner());
-        _;
+        // STRATEGY = address(0x0);
     }
 
     function totalAssets() public view override returns (uint256) {
@@ -43,7 +37,7 @@ import "./utils/FixedPointMathLib.sol";
     }
 
     //sets the strategy address to send funds
-    //Funds get sent to strategy address
+    //Funds get sent to strategy address(controlled by bot)
     function setStrategy(address strategy) public onlyOwner returns (bool) {
         STRATEGY = strategy;
         return true;
@@ -55,7 +49,7 @@ import "./utils/FixedPointMathLib.sol";
         BUFFER = buffer;
         return true;
     }
-
+    //returns current balance of underlying in vault (represents buffer amount)
     function bufBalance() public view returns (uint256) {
         return UNDERLYING.balanceOf(address(this));
     }
@@ -64,28 +58,19 @@ import "./utils/FixedPointMathLib.sol";
     function afterDeposit(uint256 amount) internal virtual override {
         //todo logic to distribute funds to Strategy (for bot)
         UNDERLYING.safeTransfer(STRATEGY, amount);
-        //increment balance of sender
-        balances[msg.sender] += amount;
     }
 
     //Claims rewards and sends funds from the Harvester to Vault
     function beforeWithdraw(uint256 amount) internal virtual override {
         if (UNDERLYING.balanceOf(address(this)) >= amount) {
             UNDERLYING.safeTransfer(STRATEGY, amount);
-            //increment balance of sender
-            balances[msg.sender] += amount;
             TOP_UP = 0;
         } else {
             //todo throw error
             TOP_UP = amount - UNDERLYING.balanceOf(address(this));
-            revert();
         }
         require(UNDERLYING.balanceOf(address(this)) >= amount, "Insufficient funds in vault");
         previewWithdraw(amount);
-        balances[msg.sender] -= amount;
     }
 
-    function isOwner() public view returns (bool) {
-        return msg.sender == owner;
-    }
 }
