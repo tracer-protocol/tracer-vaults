336d554383b4b65ee68200e40ee009d4f6a069f3
raymogg <raymondmogg@gmail.com>
2022-03-30 14:03:45 +0200

    add only owner safety functions and setter functions

--- contracts/TokeVault.sol
+++ contracts/TokeVault.sol
@@ -6,41 +6,39 @@ import {FixedPointMathLib} from "../lib/solmate/src/utils/FixedPointMathLib.sol"
 import "./interfaces/tokemak/ILiquidityPool.sol";
 import "./interfaces/tokemak/IRewards.sol";
 import "./interfaces/uniswap/UniswapV2Router02.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 /**
  * A Tokemak compatible ERC4626 vault that takes in a tAsset and auto compounds toke rewards
  * received from staking that tAsset in toke.
  */
-contract TokeVault is ERC4626 {
+contract TokeVault is ERC4626, Ownable {
     using FixedPointMathLib for uint256;
     using SafeTransferLib for ERC20;
 
-    // require assets
+    // required assets
     ERC20 public immutable toke;
     ERC20 public immutable tAsset;
 
-    // tokemak liquidity pool
-    ILiquidityPool public tokemakPool;
-    IRewards public rewards;
+    // tokemak contracts
+    ILiquidityPool public immutable tokemakPool;
+    IRewards public immutable rewards;
 
+    // CONFIGURABLE PARAMS
     // keeper rewards: default of 1.5 Toke
     uint256 public keeperRewardAmount = 1500000000000000000;
-
-    // user requested withdraws
-    mapping(address => uint256) public requestedWithdraws;
+    // max swap amount for uniswap trade. This amount can be swapped every swapCooldown hours
+    uint256 public maxSwapTokens = 100000000000000000000;
+    uint256 public swapCooldown = 12 hours;
+    // address that gets 9% of generated yield
+    address public feeReciever;
 
     // uniswap
     address public immutable swapRouter;
     address[] public tradePath;
-    // configurable max swap amount.
-    uint256 public maxSwapTokens = 100000000000000000000;
     uint24 public swapPoolFee = 3000; // default 0.3%
     uint256 public lastSwapTime = 0;
-    uint256 public swapCooldown = 12 hours;
-
-    // address that gets 9% of generated yield
-    address public feeReciever;
 
     /**
      * @param _tAsset the tAsset that this vault handles
@@ -128,6 +126,7 @@ contract TokeVault is ERC4626 {
         // deposit all of underlying back into toke and take service fee
         uint256 underlyingBal = underlying.balanceOf(address(this));
         // deposit 90% back into toke, take 1% keeper fee, take 9% service fee.
+        // todo: This could be configurable in the future
         uint256 depositAmount = underlyingBal - (underlyingBal / 10); // 90%
         uint256 keeperFee = underlyingBal/100;
         uint256 serviceFee = underlyingBal - depositAmount - keeperFee;
@@ -151,5 +150,39 @@ contract TokeVault is ERC4626 {
         bool hasTimePassed = block.timestamp > lastSwapTime + swapCooldown;
         bool hasBalance = toke.balanceOf(address(this)) != 0;
         return hasTimePassed && hasBalance;
+    }
+
+    /**
+    * @notice THIS SAFETY FUNCTION IS ONLY FOR TESTING IN PROD
+    * @dev remove once contract is audited and verified. This allows the owner to
+    * withdraw any asset to themself. USE WITH CAUTION
+    */
+    function withdrawAssets(address asset, uint amount) external onlyOwner {
+        ERC20(asset).safeTransfer(owner(), amount);
+    }
+
+    /**
+    * @notice allows the keeper reward to be modified
+    */
+    function setKeeperReward(uint newKeeperReward) external onlyOwner {
+        keeperRewardAmount = newKeeperReward;
+    }
+
+    /**
+    * @notice allows the max swap tokens to be modified
+    */
+    function setMaxSwapTokens(uint newMaxSwapTokens) external onlyOwner {
+        maxSwapTokens = newMaxSwapTokens;
+    }
+
+    /**
+    * @notice allows the max swap tokens to be modified
+    */
+    function setSwapCooldown(uint newSwapCooldown) external onlyOwner {
+        swapCooldown = newSwapCooldown * 1 hours;
+    }
+
+    function setFeeReciever(address newFeeReciever) external onlyOwner {
+        feeReciever = newFeeReciever;
     }
 }
