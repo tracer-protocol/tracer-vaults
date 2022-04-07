52dc595f50cf4d6603d01ac1720ccf43c708a657
raymogg <raymondmogg@gmail.com>
2022-04-07 17:35:08 +0200

    reset config

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,15 +19,15 @@ module.exports = {
         deployer: 0,
     },
     networks: {
-        deploy: {
-            url: process.env.RPC_URL,
-            accounts: [process.env.PK],
-        },
-        test: {
+        hardhat: {
             forking: {
                 url: process.env.RPC_URL,
                 blockNumber: 14448270,
             },
+        },
+        deploy: {
+            url: process.env.RPC_URL,
+            accounts: [process.env.PK],
         },
     },
     etherscan: {
